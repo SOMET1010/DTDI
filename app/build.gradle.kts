ad67fb3a6e45e758a7f8f7eaad1f7c67a01b67b3
PATRICK <141404422+SOMET1010@users.noreply.github.com>
2026-08-25 07:48:21 +0000

    Bump PASS Academy to v1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "ci.ansut.passacademy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.0"
+        versionCode = 13
+        versionName = "1.3.0"
     }
 }
